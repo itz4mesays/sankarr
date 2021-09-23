1c50099f9bb6414b0641f3deb9a35d34470a9073
Oyedele Olufemi <itz4mesays@gmail.com>
2021-09-23 18:32:34 +0100

    new commit

--- config/adminlogged.js
+++ config/adminlogged.js
@@ -1,13 +1,12 @@
 const admin = {
     name: 'Sankar Prakash',
-    email: 'sankarapp@gmail.com',
-    password: 'sankarapp123',
+    email: 'billahranga@gmail.com',
     role: 'Admin'
 }
 
 
 const adminLogged = (req, res, next) => {
-    if(admin.email == 'sankarapp@gmail.com' && admin.role == 'Admin'){
+    if(req.params.email == admin.email && admin.role == 'Admin'){
         req.user = admin
         next() 
     }else{
--- routes/admin.router.js
+++ routes/admin.router.js
@@ -5,7 +5,7 @@ const { adminLogged } = require('../config/adminlogged')
 const UserEnv = require('../app/models/user_env')
 const ConfigSite = require('../app/models/config_site')
 
-router.get('/list-users', adminLogged, adminController.list_users)
-router.post('/update', adminLogged, adminController.update)
+router.get('/list-users/:email', adminLogged, adminController.list_users)
+router.post('/update/', adminController.update)
 
 module.exports = router
