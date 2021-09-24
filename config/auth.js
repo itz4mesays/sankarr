84c175a50cc4897fc10b65036b1dbe98cf8cc57f
Sankar Prakash <billahranga@gmail.com>
2021-09-24 23:41:48 +0530

    Initial commit on proj

--- config/auth.js
+++ config/auth.js
@@ -6,12 +6,12 @@ const GoogleStrategy = require( 'passport-google-oauth2').Strategy
 const User = require('../app/models/user')
 
 // callbackURL: 'http://localhost:5000/google/callback',
-// callbackURL: 'https://sankarapp.herokuapp.com/google/callback',
+// callbackURL: 'https://shanapps.herokuapp.com/google/callback',
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:5000/google/callback',
+    callbackURL: 'https://shanapps.herokuapp.com/google/callback',
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
