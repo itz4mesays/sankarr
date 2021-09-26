df9f2ed0d35a37292870f1c73bb9401a7374e53c
Sankar Prakash <billahranga@gmail.com>
2021-09-26 12:16:39 +0530

    Issue fixes

--- config/message.js
+++ config/message.js
@@ -14,7 +14,7 @@ const sendButtonMessage = (sender, token, data) => {
           "payload": data
         }
     }
-    messageData = JSON.stringify(JSON.parse(messageData))
+    messageData = JSON.parse(messageData)
     console.log("ButtonmessageData:" + messageData)
     sendAutoMessage(sender, token, messageData)
 }
