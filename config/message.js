e05b18bbb693c9c442fc7876a1157ecff3e14d2d
Sankar Prakash <billahranga@gmail.com>
2021-09-26 11:17:13 +0530

    Issue fixes

--- config/message.js
+++ config/message.js
@@ -6,66 +6,7 @@ const sendTextMessage = (sender, token, data) => {
   }
   sendAutoMessage(sender, token, messageData)
 }
-const sendButtonMessage = (sender, token, data) => {
-    messageData = {
-        "attachment":{
-          "type":"template",
-          "payload": data
-        }
-    }
-    sendAutoMessage(sender, token, messageData)
-}
-const sendImageMessage = (sender, token, data) => {
-    messageData = {
-      "attachment":{
-        "type":"template",
-        "payload":{
-          "template_type": "media",
-          "elements": data
-        }
-      }
-    }
-    sendAutoMessage(sender, token, messageData)
-}
-const sendVideoMessage = (sender, token, data) => {
-    messageData = {
-      "attachment":{
-        "type":"template",
-        "payload":{
-          "template_type": "media",
-          "elements": data
-        }
-      }
-    }
-    sendAutoMessage(sender, token, messageData)
-}
-const sendFeedbackMessage = (sender, token, data) => {
-    messageData = {
-      "attachment":{
-          "type":"template",
-          "payload": data
-      }
-    }
-    sendAutoMessage(sender, token, messageData)
-}
-const sendGenericMessage = (sender, token, data) => {
-    messageData = {
-      "attachment": {
-          "type": "template",
-          "payload": data
-      }
-    }
-    sendAutoMessage(sender, token, messageData)
-}
-const sendOrderMessage = (sender, token, data) => {
-    messageData = {
-    "attachment":{
-        "type":"template",
-        "payload":data
-    }
-  }
-    sendAutoMessage(sender, token, messageData)
-}
+
 
 const sendAutoMessage = (sender, token, messageData) => {
   request({
@@ -76,7 +17,7 @@ const sendAutoMessage = (sender, token, messageData) => {
         recipient: {id:sender},
         message: messageData,
     }
-  }, const(error, response, body) {
+  }, function(error, response, body) {
       if (error) {
           console.log('Error sending messages: ', error)
       } else if (response.body.error) {
@@ -86,12 +27,6 @@ const sendAutoMessage = (sender, token, messageData) => {
 }
 module.exports = {
     sendTextMessage,
-    sendButtonMessage,
-    sendImageMessage,
-    sendVideoMessage,
-    sendFeedbackMessage,
-    sendGenericMessage,
-    sendOrderMessage,
     sendAutoMessage
 }
 // module.exports.adminLogged = adminLogged
