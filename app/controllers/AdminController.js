e3d68edb1bad28efacee7010d8c5324a9ff350b8
Sankar Prakash <billahranga@gmail.com>
2021-09-26 11:03:00 +0530

    Issue fixes

--- app/controllers/AdminController.js
+++ app/controllers/AdminController.js
@@ -90,31 +90,31 @@ module.exports = {
             requestType = configSite.rtype
             if (requestType === 'text') {
                 sendTextMessage(sender, token, responseData.substring(0, 200))
-                continue
+                // continue
             }
             if (requestType === 'button') {
                 sendButtonMessage(sender, token, responseData)
-                continue
+                // continue
             }
             if (requestType === 'image') {
                 sendImageMessage(sender, token, responseData)
-                continue
+                // continue
             }
             if (requestType === 'video') {
                 sendVideoMessage(sender, token, responseData)
-                continue
+                // continue
             }
             if (requestType === 'feedback') {
                 sendFeedbackMessage(sender, token, responseData)
-                continue
+                // continue
             }
             if (requestType === 'grid') {
                 sendGenericMessage(sender, token, responseData)
-                continue
+                // continue
             }
             if (requestType === 'order') {
                 sendOrderMessage(sender, token, responseData)
-                continue
+                // continue
             }
           }).catch(err => {
               return res.json({statusCode: 400, message: err})
