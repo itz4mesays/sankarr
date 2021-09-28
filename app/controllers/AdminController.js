577dd4b6432c558c7ceaf68f6fffc81f9a351c04
Sankar Prakash <billahranga@gmail.com>
2021-09-28 22:07:07 +0530

    Logged event dets

--- app/controllers/AdminController.js
+++ app/controllers/AdminController.js
@@ -103,6 +103,8 @@ module.exports = {
       messaging_events = req.body.entry[0].messaging
       for (i = 0; i < messaging_events.length; i++) {
         event = req.body.entry[0].messaging[i]
+        console.log(event)
+        console.log(event.postback)
         console.log(event.postback.payload)
         sender = event.sender.id
         recipient = event.recipient.id
